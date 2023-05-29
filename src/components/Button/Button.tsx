0f816d0b4e565f912716db577e1e9306140fcd02
Nel <nel.renaudin@gmail.com>
2023-05-29 09:38:47 +0200

    Style Button primary and outline on hover

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,16 @@
+import './Button.sass'
+
+interface ButtonProps {
+  children: string | JSX.Element | JSX.Element[]
+  type?: string
+  size?: string
+  handleClick: () => unknown | void
+}
+
+function Button({children, type="dark", size="medium", handleClick}: ButtonProps) {
+  return (
+    <button className={`Button ${type} ${size}`} onClick={handleClick}>{children}</button>
+  )
+}
+
+export default Button
