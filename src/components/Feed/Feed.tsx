4ac0041fad9a153f382346b64b89aef5c571a42c
Nel <nel.renaudin@gmail.com>
2023-06-07 08:37:11 +0200

    Refactor Feed

--- src/components/Feed/Feed.tsx
+++ src/components/Feed/Feed.tsx
@@ -1,17 +1,25 @@
-import { TweetRef } from '../../types'
+import { collection, orderBy, query, where } from 'firebase/firestore'
+import { db } from '../../firebase'
+import { useCollectionData } from 'react-firebase-hooks/firestore'
 import Tweet from '../Tweet/Tweet'
+import Loader from '../Loader/Loader'
 import './Feed.sass'
 
 type FeedProps = {
-  tweets: TweetRef[]
-  userId?: string
+  userIds: string[]
 }
 
-const Feed: React.FC<FeedProps> = ({ tweets, userId }) => {
+const Feed: React.FC<FeedProps> = ({ userIds }) => {
+  const feedCollection = collection(db, 'feed')
+  const feedQuery = query(feedCollection, where('userId', 'in', userIds), orderBy('timestamp', 'desc'))
+  const [feed, loading] = useCollectionData(feedQuery)
+
+  if (loading) return <Loader />
+
   return (
     <div className="Feed">
-      {tweets.map((tweet) => (
-        <Tweet key={tweet.tweetId} tweetId={tweet.tweetId} retweetedBy={tweet.retweetedAt ? userId : null} />
+      {feed?.map((item) => (
+        <Tweet key={item.id} tweetId={item.tweetId} retweetedBy={item.type === 'retweet' ? item.userId : null} />
       ))}
     </div>
   )
